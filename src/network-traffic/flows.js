b5da2d808f4ceb371c9360edd895aa46941506c5
Tomer <tomer@lightlytics.com>
2024-02-16 02:09:42 +0200

    Initial commit

---
+++ src/network-traffic/flows.js
@@ -0,0 +1,167 @@
+const { Timestamp } = require('google-protobuf/google/protobuf/timestamp_pb')
+const {
+  flowLogsMsgProto,
+  FlowLogsDeviceTypeEnum,
+  FlowLogActionEnum,
+} = require('../models/protobuf/proto')
+
+const flowCategoryInfo = {
+  NetworkSecurityGroupFlowEvent: {
+    type: 'azure_network_security_group',
+    resourceId: 'resourceId',
+    properties: 'properties',
+    flows: 'flows',
+    flowGroups: 'flows',
+    // fields https://learn.microsoft.com/en-us/azure/network-watcher/media/network-watcher-nsg-flow-logging-overview/tuple.png
+    fields: [
+      'start',
+      'srcaddr',
+      'dstaddr',
+      'srcport',
+      'dstport',
+      'protocol', // U / T
+      'flow_direction', // I / O
+      'traffic_decision', // A / D
+      'flow_state', // B / C / E
+      'packets_sent',
+      'bytes_sent',
+      'packets_received',
+      'bytes_received',
+    ],
+  },
+  FlowLogFlowEvent: {
+    type: 'azure_virtual_network',
+    resourceId: 'targetResourceID',
+    properties: 'flowRecords',
+    flows: 'flows',
+    flowGroups: 'flowGroups',
+    // fields https://learn.microsoft.com/en-us/azure/network-watcher/media/vnet-flow-logs-overview/vnet-flow-log-format.png#lightbox
+    fields: [
+      'start',
+      'srcaddr',
+      'dstaddr',
+      'srcport',
+      'dstport',
+      'protocol', // 6 - TCP, 17 - UDP
+      'flow_direction',
+      'flow_state', // B / C / E / [D]enied
+      'flow_encryption',
+      'packets_sent',
+      'bytes_sent',
+      'packets_received',
+      'bytes_received',
+    ],
+  },
+}
+
+const extractor = (type, data) => {
+  const flowInfo = flowCategoryInfo[type]
+  return data[flowInfo.properties].flows.reduce(
+    (acc, flow) => [
+      ...acc,
+      ...flow[flowInfo.flowGroups].reduce(
+        (acc, tuple) => [
+          ...acc,
+          ...tuple.flowTuples.map(t => ParseLog(t, flowInfo)),
+        ],
+        [],
+      ),
+    ],
+    [],
+  )
+}
+
+const ParseLog = (rawLog, flowInfo) => {
+  const log = {}
+  rawLog.split(',').forEach((value, fieldIndex) => {
+    const fieldName = flowInfo.fields[fieldIndex]
+    log[fieldName] = value
+  })
+
+  return log
+}
+
+const ParseFlows = data => {
+  const logs = []
+  const category = data.records?.[0]?.category
+  const resourceIdField = flowCategoryInfo[category].resourceId
+  const resourceId = data.records?.[0][resourceIdField]
+
+  const flows = data.records.reduce(
+    (acc, record) => [...acc, ...extractor(record.category, record)],
+    [],
+  )
+
+  flows.forEach(log => {
+    if (log.bytes_received) {
+      logs.push(FlowlogConverter(SwapLogDirection(log)))
+    }
+    logs.push(FlowlogConverter(log))
+  })
+
+  const accountIdString = resourceId.split('/')[2]
+
+  return {
+    accountIdString,
+    deviceType: FlowLogsDeviceTypeEnum.values.AZURE_FLOW_LOGS,
+    vpcId: category === 'FlowLogFlowEvent' ? resourceId : '',
+    deviceId: flowCategoryInfo[category]?.type || category,
+    logs,
+  }
+}
+
+const SwapLogDirection = log => {
+  const swappedLog = { ...log }
+  swappedLog.srcaddr = log.dstaddr
+  swappedLog.dstaddr = log.srcaddr
+  swappedLog.srcport = log.dstport
+  swappedLog.dstport = log.srcport
+  swappedLog.bytes_sent = log.bytes_received
+  swappedLog.bytes_received = log.bytes_sent
+
+  return swappedLog
+}
+
+const FlowlogConverter = ({
+  start,
+  srcaddr,
+  dstaddr,
+  srcport,
+  dstport,
+  protocol,
+  traffic_decision,
+  flow_state,
+  bytes_sent,
+}) => {
+  const date = new Date(start * 1000)
+  const protocol_code = protocol === 'T' || protocol === 6 ? 6 : 17
+  const action =
+    traffic_decision === 'D' || flow_state === 'D'
+      ? FlowLogActionEnum.values.REJECT
+      : FlowLogActionEnum.values.ACCEPT
+  return flowLogsMsgProto.create({
+    start: dateToProtoTimestamp(date),
+    end: dateToProtoTimestamp(date),
+    srcaddr,
+    dstaddr,
+    srcport: Number(srcport),
+    dstport: Number(dstport),
+    protocol: {
+      protocol: protocol === 'T' || protocol === 6 ? 'TCP' : 'UDP',
+      protocol_code,
+    },
+    bytes: Number(bytes_sent),
+    action,
+  })
+}
+
+function dateToProtoTimestamp(date) {
+  const timeStamp = new Timestamp()
+  timeStamp.fromDate(date)
+  return timeStamp.toObject()
+}
+
+module.exports = {
+  flowCategoryInfo,
+  ParseFlows,
+}
